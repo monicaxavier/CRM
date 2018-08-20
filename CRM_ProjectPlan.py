63a175508c884cc9343d1cb0072c2d04235e1da1
monicaxavier <30355809+monicaxavier@users.noreply.github.com>
2018-08-20 07:05:27 +0530

    Project Plan

---
+++ CRM_ProjectPlan.py
@@ -0,0 +1,4 @@
+#This Document is for BrainStorming
+#Project Completion Estminate Date: December 2018
+#Rick uses Principles of Software Engineering to build this simple CRM
+#Rick plans to understand Ian Sommerville's thoughts and borrow from different disciplines 
