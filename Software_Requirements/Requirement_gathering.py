56eaa7dd0509aa734ae2e07275b40a2075e3273c
monicaxavier <30355809+monicaxavier@users.noreply.github.com>
2018-08-21 06:47:49 +0530

    Updated Requirements for software

---
+++ Software_Requirements/Requirement_gathering.py
@@ -0,0 +1,36 @@
+#This File is a draft of Software Requirements
+#Ian Sommerville
+"""
+
+Stakeholder -- Client Invested in Software product, Product Owner
+
+User_requirements
+
+System_requirements
+
+Functional Requirements: How the system should interact and behave in different
+situations. What the System is supposed to do.Eg: Functions of Toothbrush?
+To Brush the Teeth.
+
+Non-Functional Requirements: Constrains by the system.
+This includes reliability, time, scalibility, peformance,
+security. Sometimes, this is more important.Eg: What is Non-Functional
+Requirement of Toothbrush? But Toothbrush breaks after you brush your teeth.
+[Make sure we can quantitavely test out the Non-Functional Requirement]
+[Ease of Use: Training Time, Number of Help Frames]
+
+Product_Requirement: This CRM will be available to Church Members (Mon-Sunday)
+
+Organizational_Requirement: Church members will be able to access this product.
+--------------------
+To_do:
+Software_Requirement_Document:
+Scenario Collection:
+Use Case:
+Ethnography: Cultural and Systematic study. How the software product would be supposed
+in a cultural and work context.
+Requirement Validation: a) Consistency b) Validation c) Completeness d) Realism Check
+
+
+
+"""
