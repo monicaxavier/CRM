18ca575028232985e2900193f27017ee482cf259
monicaxavier <30355809+monicaxavier@users.noreply.github.com>
2018-08-22 08:11:51 +0530

    Sample programs

--- Python_Sample/Sample1.py
+++ Python_Sample/Sample1.py
@@ -1,4 +1,30 @@
 #Simple explanation of Python
 
+friends = ['john','pat','gary']
 
-print "Hello World"
+enemy = {'muthu':'raja','kumar':'saravanan'}
+
+#Loop, C, C++, Python, PERL, Clojure
+#India, Technology, Language
+#Absract and learn important concepts
+#In Python, Loop is For Loop, While Loop, Nested Loop
+#In PERL, Loop is x, y, z, m.
+# Dictionary: Key, Value
+
+#for [element] in [variable]
+#print [element]
+
+
+#function = MAthematics
+# Input = Output
+# Fan, (Switch on) = Fan suthudhu
+
+def greet(name):
+    """
+    This is a function.
+    """
+    print "Kutti", name
+
+
+greet("Payan")
+greet("Ponnu")
---
+++ Python_Sample/Sample7.py
@@ -0,0 +1,15 @@
+#Tutorial
+
+#Java, C, C++, Python, PERL,
+#Import Libraries.
+#Why? Because, we don't write from scratch.
+#Some Programmers might write specific Libraries.
+#Math library open, how?
+# Sys nu oru library irukum.
+# Sys. kudukumbdhu, we are gettting function.
+#What is Function? Specific task, X->Y
+#Number, Number.add(), Number.subtract(), Number.divide()
+#Python build --- Web, applications, NLP, Natural Language Process,
+#what is NLP? Speech Recognition, SIRI, Android Voice, You could expand
+#to any languge. What is important? Knowing how to build a house, i.e. Software
+#
