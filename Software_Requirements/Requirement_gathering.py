872637583579fc60b0f6b81ca3ae98d4173d0ce1
monicaxavier <30355809+monicaxavier@users.noreply.github.com>
2018-08-22 07:05:51 +0530

    Draft of Proposal

---
+++ Software_Requirements/Requirement_gathering.py
@@ -0,0 +1,77 @@
+#This File is a draft of Software Requirements
+#Ian Sommerville
+
+#Author: Monica, Rick Rejeleene
+#Date: August 21, 2018:
+"""
+
+Stakeholder -- Client Invested in Software product, Product Owner
+
+User_requirements
+
+System_requirements
+
+Functional Requirements: How the system should interact and behave in different
+situations. What the System is supposed to do.Eg: Functions of Toothbrush?
+To Brush the Teeth.
+
+Non-Functional Requirements: Constrains by the system.
+
+This includes reliability, time, scalibility, peformance,
+security. Sometimes, this is more important.Eg: What is Non-Functional
+Requirement of Toothbrush? But Toothbrush breaks after you brush your teeth.
+[Make sure we can quantitavely test out the Non-Functional Requirement]
+[Ease of Use: Training Time, Number of Help Frames]
+
+Product_Requirement: This CRM will be available to Church Members (Mon-Sunday)
+
+Organizational_Requirement: Church members will be able to access this product.
+--------------------
+To_do:
+Software_Requirement_Document:
+Scenario Collection:
+Use Case:
+Ethnography: Cultural and Systematic study. How the software product would be supposed
+in a cultural and work context.
+Requirement Validation: a) Consistency b) Validation c) Completeness d) Realism Check
+
+"""
+"""
+System Modelling:
+
+Kallamozhi:
+
+MAthematics: Circle, Number,
+
+Abstract: Men
+
+John, Fioz, Muthu
+Concrete: Real World existing
+
+Documentation:
+Verbalize the whole process
+
+Benefits -- Concrete knowledge of Software
+            Others can read your Software
+            Easy maintainence
+
+
+Idea -- CRM
+[
+.........
+.........
+.........
+.........
+
+]
+
+
+# IDEA: Abstract
+#Concrete:
+#
+
+1) Requirements, Idea formulate -- Proposal,
+Requirements -- System Requirements, Design, Architectural,
+Software [Prototype], Implementation, Testing, Review.
+
+"""
