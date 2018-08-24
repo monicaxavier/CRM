0ae3712c4288f6ee718f30061fe190b91ff16390
ludwigwittgenstein2 <penpals.oranges14@gmail.com>
2018-08-23 20:22:49 -0400

    update on requirements

---
+++ 1-Software_Requirements/Requirement_gathering_2.py
@@ -0,0 +1,43 @@
+#This File is a draft of Software Requirements
+#Ian Sommerville
+
+#Author: Monica, Rick Rejeleene
+#Date: August 23, 2018:
+
+"""
+The CRM market is flooded with many applications. We want to aim to do our best in this. The rise of
+SaaS has turned software industry into a booming business. There are many issues within SaaS.
+However -- the industry is not like manufacturing or airline industry.
+
+The is no perfect software, yet we want software to be flexible to meet the needs of many.
+
+User problems of Software:
+Too complex
+Too many features
+Unintuitive
+Bugs
+Takes too long to wait
+Many screens, When we are shifting to many tabs in browsing, our attention loses from
+the content.
+
+Computer Science problems of Software:
+Scaling the software
+Response rate, optimization
+Complexity
+
+When I was in Orlando, Florida. I got 50 emails a day. I could not manage them. I felt
+frustrated, anxious and wanted to burn my email box. I even deleted all emails sent from
+CRU. I just did not know how to?
+
+But I knew who would hold the solution for it. It was the President of Cru's assistant.
+I met him for coffee. I badly wanted to learn his skill. He didn't know me but I was comfortable
+meeting new people and becoming friends every-day. I showed him my problem. My email box was flooded,
+I wanted to yell and scream at CRU for sending me automation emails. He showed how he labeled the President's email.
+
+In three months, I learnt that skill and I felt extremely happy. I thanked him so much.
+
+The Software is there but I did not know how to use it efficiently? It took few months to learn basics of
+organizing an email. 
+
+
+"""
