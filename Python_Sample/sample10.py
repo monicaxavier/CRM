96670adaf488262edd277abc738e89a1c53775ea
monicaxavier <30355809+monicaxavier@users.noreply.github.com>
2018-08-28 07:18:18 +0530

    More ideas

--- Python_Sample/sample10.py
+++ Python_Sample/sample10.py
@@ -1,5 +1,30 @@
-#Homework -- 
+#Homework --
 #List Your favorite Software
 #Why do you like it?
 #Why do people use it?
 #What problem does it solve?
+
+#What problem do people in Kodaikanal Face?
+#What is the journey of a traveller in Kodaikanl?
+#Web-application -->
+
+##########################
+
+# Businss to Consumer market
+#Binsess to Business
+#[Tirunelveli] -- [Kodaikanal]
+#[Traffic], [Hotel], [Which place to visit]
+#Nobody has targeted small hotels.
+#Small hotels -- Booking revenue. rs.1000
+#People have Mobile Phones. SMS vatchu booking?
+# Per booking 10% charge.
+# Taml Nadu society problems, complex problems.
+# 50 contact oru problem solvve, 5 steps.
+#[Google-maps],[Manybooking],[Trip-Advisor]
+# Nobody is in the market... You will benefit.
+# CRM develop, It has be flexible for wedding, marriage.
+# Wedding per wedding 20,000 --
+# Zoho --> Finance,  Messaging, Business,
+#CRM -->  Adapt to every industry/type
+
+#################
