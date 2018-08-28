04d3816c1c22a0768dc6302736260f99eda5ee34
ludwigwittgenstein2 <penpals.oranges14@gmail.com>
2018-08-27 21:07:23 -0400

    More Python

--- Python_Sample/sample9.py
+++ Python_Sample/sample9.py
@@ -1,19 +1,11 @@
 import plotly.graph_objs as go
-import plotly.plotly as py
+fig = go.FigureWidget()
+# Display an empty figure
+fig
 
-import numpy as np
-
-colorscale = [[0, '#FAEE1C'], [0.33, '#F3558E'], [0.66, '#9C1DE7'], [1, '#581B98']]
-trace1 = go.Scatter(
-    y = np.random.randn(500),
-    mode='markers',
-    marker=dict(
-        size='16',
-        color = np.random.randn(500),
-        colorscale=colorscale,
-        showscale=True
-    )
-)
-data = [trace1]
-url_1 = py.plot(data, filename='scatter-for-dashboard', auto_open=False)
-py.iplot(data, filename='scatter-for-dashboard')
+# Add a scatter chart
+fig.add_scatter(y=[2, 1, 4, 3])
+# Add a bar chart
+fig.add_bar(y=[1, 4, 3, 2])
+# Add a title
+fig.layout.title = 'Hello FigureWidget'
