58ed53d337c3c9245117283f0db0a356a03e3fd7
ludwigwittgenstein2 <penpals.oranges14@gmail.com>
2018-09-06 11:07:24 -0400

    changes

--- Python_Sample/sample12.py
+++ Python_Sample/sample12.py
@@ -1,91 +1,91 @@
-Tuple(Collection)
---> same like List but it cannot be changed
-ex1:
+#Tuple(Collection)
+#--> same like List but it cannot be changed
+#ex1:
 Tuple = ('John','Xavier','Emalda')
 print(Tuple)
---->('John','Xavier','Emalda')
+#--->('John','Xavier','Emalda')
 
-Accessing tuples
-print(Tuple[1])--> Xavier
+#Accessing tuples
+#print(Tuple[1])--> Xavier
 
-We cannot change the values in the tuples as we did in list. even if you try to change the value is still the same.
+#We cannot change the values in the tuples as we did in list. even if you try to change the value is still the same.
 
-looping
+#looping
 mytuple = ('apple','orange','grapes')
 for k in mytuple:
 	print(k)
-->
-apple
-orange
-grapes
+#->
+#apple
+#orange
+#grapes
 
-Tuple length
+#Tuple length
 mytuple = ('apple','orange','grapes')
 print(len(mytuple))
--->3
+#-->3
 
-Tuples are unchangeable, you cannot add/remove/update the tuple but you can delete the tuple completely.
-using del keyword
+#Tuples are unchangeable, you cannot add/remove/update the tuple but you can delete the tuple completely.
+#using del keyword
 mytuple = ('apple','orange','grapes')
 del(mytuple)
 
-we can create a tuple using tuple() method
-mytuple = tuple(('mango','pomo'))
+#we can create a tuple using tuple() method
+#mytuple = tuple(('mango','pomo'))
 for x in mytuple:
   print(x)
--->
-mango
-pomo
+#-->
+#mango
+#pomo
 
-Tuple methods
-it has two built in methods
-count()
-index()
+#Tuple methods
+#it has two built in methods
+#count()
+#index()
 
-count()
+#count()
 mytuple = (1,2,6,8,4,2,7,9,2)
 k=mytuple.count(2)
 print(k)
-ans -> 3
+#ans -> 3
 
-index()-->return the first occurance position of the given value
+#index()-->return the first occurance position of the given value
 mytuple = (1,2,6,8,4,2,7,9,2)
 k=mytuple.index(2)
 print(k)
-ans-->1
+#ans-->1
 
-Set
-same like other collection.
-loop through the set
+#Set
+#same like other collection.
+#loop through the set
 myset={'apple','banana','orange'}
 print('banana' in myset)
-ans-->true
+#ans-->true
 
-Dictionary
+#Dictionary
 mydict = {'name':'moni','age':23,'fname':'jesu'}
 for x,y in mydict.items():
   print(x,y)
--->
-name moni
-age 23
-fname jesu
+#-->
+#name moni
+#age 23
+#fname jesu
 
 mydict = {'name':'moni','age':23,'fname':'jesu'}
 for x in mydict.values():
   print(x)
-it will display values alone
-moni
-23
-jesu
+#it will display values alone
+#moni
+#23
+#jesu
 
-adding to dictionary
+#adding to dictionary
 mydict = {'name':'moni','age':23,'fname':'jesu'}
 mydict['company']='cts'
 print(mydict)
 
-to remove use del keyword
+#to remove use del keyword
 
-*****Loops in Python******
+#*****Loops in Python******
 moni = 33
 rick = 20
 if moni > rick:
@@ -93,10 +93,10 @@ if moni > rick:
 else:
   print ('moni is younger than rick')
 
-While Loop
-**********
+#While Loop
+#**********
 i = 1
 while(i<6):
   print(i)
   i +=1
-1 2 3 4 5
+#1 2 3 4 5
