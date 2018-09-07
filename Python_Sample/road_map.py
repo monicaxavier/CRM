06ad19ea229648f31c3c8b4367250694358d6e08
monicaxavier <30355809+monicaxavier@users.noreply.github.com>
2018-09-07 07:00:58 +0530

    Road map

---
+++ Python_Sample/road_map.py
@@ -0,0 +1,23 @@
+#This is learning path for CRM
+#
+
+#Software Engineering --> Requriements --> Production: 2018-2019
+
+
+
+#Software Engineering:
+
+#Requirements:
+#
+
+
+#Software tools: Python-Git-Django:
+
+#Python:
+#Function
+#Class
+#Documentation
+#Python Design Pattern
+#Features -- How to build
+#Logic of Features
+#Optimizing software
