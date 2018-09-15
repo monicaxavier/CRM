ce89c0083f9c0c0372fe8771a84fdeb0e41f0447
ludwigwittgenstein2 <penpals.oranges14@gmail.com>
2018-09-14 22:33:28 -0400

    test scripts!

--- Python_Sample/sample_goodreads_api.py
+++ Python_Sample/sample_goodreads_api.py
@@ -7,10 +7,20 @@
 #Version:0.1
 #https://www.goodreads.com/api/index
 
-
+"""
 import requests
 import json
 
 response = requests.get('https://www.goodreads.com/search.xml?key=sBITGRylosZkjIAaixQ&q=Ender%27s+Game')
 
 print response
+"""
+"""
+import sys
+
+print sys.path
+
+"""
+import os
+
+print os.environ['PYTHONPATH'].split(os.pathsep)
