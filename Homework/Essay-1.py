d7ba8f1808a971d7eaf9c1b0da32733eda201a14
ludwigwittgenstein2 <penpals.oranges14@gmail.com>
2018-09-17 21:46:56 -0400

    Sample questions

--- Homework/Essay-1.py
+++ Homework/Essay-1.py
@@ -1,3 +1,18 @@
 #Please write what you learnt here
 
 #Outline:
+
+
+"""
+Sample Questions:
+
+Rick: Why do I need to know about Data-Structure?
+Rick: What is Programming Languages
+Why do I need to know about it?
+Rick: What can I know after knowing Data-structure?
+Rick: What kind of Data-structure would Kodai-CRM need?
+What did you understand from Programming languages wikipedia?
+
+
+
+"""
