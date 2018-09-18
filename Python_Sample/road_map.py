a38b1561d92eaff77228e808f10bfbe9e64b8138
ludwigwittgenstein2 <penpals.oranges14@gmail.com>
2018-09-17 21:22:42 -0400

    Road map changes

--- Python_Sample/road_map.py
+++ Python_Sample/road_map.py
@@ -1,5 +1,5 @@
 #This is learning path for CRM
-#
+#Last updated September 18, 2018, Rick Rejeleene
 
 #Software Engineering --> Requriements --> Production: 2018-2019
 
@@ -8,16 +8,29 @@
 #Software Engineering:
 
 #Requirements:
+#Sofware-Complexity
+#Design-Philosophies
+#Software Design
+#Documentation
 #wait....
 
 
 #Software tools: Python-Git-Django:
 
+#Road map for learning and creating CRM
 #Python:
 #Function
 #Class
+#Unit Testing
+#Libraries
 #Documentation
 #Python Design Pattern
 #Features -- How to build
+##API
+##CORE-CRM
+###Features
+####Libraries
+
 #Logic of Features
+#Django-Framework
 #Optimizing software
