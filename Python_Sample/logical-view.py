74e0a721b61f2c2b83c264674ce564d1b66a887f
ludwigwittgenstein2 <penpals.oranges14@gmail.com>
2018-10-14 16:02:46 -0400

    draft thoughts on CRM

---
+++ Python_Sample/logical-view.py
@@ -0,0 +1,22 @@
+###CRM Components Modules
+#Components of CRM
+#Core-Component
+
+"""
+
+Draft thoughts on the CRM application 
+
+What theory of jobs to be done using the CRM?
+-Organization of information
+-Fast
+-Reliable
+
+Features:
+-Less Complexity on Users:
+Users don't want to waste time on learning
+-Fast and Quick
+
+
+
+
+"""
