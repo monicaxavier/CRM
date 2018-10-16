9ffc98e04537b90044e9d732447c8338a0a74305
monicaxavier <30355809+monicaxavier@users.noreply.github.com>
2018-10-16 11:59:09 +0530

    Update PythonModules.py

--- Python_Sample/PythonModules.py
+++ Python_Sample/PythonModules.py
@@ -1,15 +1,16 @@
 #Module is nothing but a python library
 #we can save the module as .py and import in our code.
-Example
-Save this code in a file named mymodule.py
+"""Example
+Save this code in a file named mymodule.py """
 
 def greeting(name):
   print("Hello, " + name)
-Use a Module
+
+  """Use a Module
 Now we can use the module we just created, by using the import statement:
 
 Example
-Import the module named mymodule, and call the greeting function:
+Import the module named mymodule, and call the greeting function:"""
 
 import mymodule
 
